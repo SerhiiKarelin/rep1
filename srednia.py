c65633df91500a94e0e0299d18721a5e4720a23b
Serhii <serhii.karelin@smcebi.edu.pl>
2016-10-24 11:40:43 +0200

    Docstringi sie zmianily. Pojawila sie nowa zmiena

--- srednia.py
+++ srednia.py
@@ -2,9 +2,12 @@ import math
 from myerror import *
 
 def srednia_kwad(lista):
+    """Zmiana"""
+    """Zmiana nowa"""
     if not type(lista) == list:
         raise IBeBack()
     suma = 0
+    zmiena = 2
     for i in lista:
         suma += i*i
     return math.sqrt(float(suma)/len(lista))
